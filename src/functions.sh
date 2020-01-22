a61b10e3fb69650dadc30474cbb24d7425abe4d0
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2020-01-22 01:08:37 -0600

    find b=0

--- src/functions.sh
+++ src/functions.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-# Load bvals from file to array
-bval_file=DTI_2x32_35.bval
-read -a bvals <<< "$(cat ${bval_file})"
+function find_zero_bvals {
 
-# Find 0-based index of volumes with b=0
-zinds=()
-for i in "${!bvals[@]}"; do
-  if [[ ${bvals[i]} = 0 ]] ; then
-    zinds+=($i)
-  fi
-done
-echo "Found b=0 in ${bval_file} at ${zinds[@]}"
+  # Load bvals from file to array
+  bval_file="${1}"
+  read -a bvals <<< "$(cat ${bval_file})"
+
+  # Find 0-based index of volumes with b=0
+  zinds=()
+  for i in "${!bvals[@]}"; do
+    if [[ ${bvals[i]} = 0 ]] ; then
+      zinds+=($i)
+    fi
+  done
+  echo ${zinds[@]}
+
+}
+
+read -a zinds <<< "$(find_zero_bvals DTI_2x32_36.bval)"
 
